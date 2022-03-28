9b4d95e0ec48586ea9eda2bcfeee342a2954d492
Giovanni Barbiero <barbiero.giovanni00@gmail.com>
2022-03-28 14:07:52 +0200

    renamed UML and added image version on deliveries

--- src/main/java/it/polimi/ingsw/enums/CharacterType.java
+++ src/main/java/it/polimi/ingsw/enums/CharacterType.java
@@ -5,76 +5,76 @@ import it.polimi.ingsw.model.cards.*;
 public enum CharacterType {
     FRIAR {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Friar();
         }
     },
     FARMER {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Farmer();
         }
     },
     HERALD {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Herald();
         }
     },
     MAILMAN {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Mailman();
         }
     },
     HERBALIST {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Herbalist();
         }
     },
     CENTAUR {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Centaur();
         }
     },
     JESTER {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Jester();
         }
     },
     KNIGHT {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Knight();
         }
     },
     HARVESTER {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Harvester();
         }
     },
     MINSTREL {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Minstrel();
         }
     },
     PRINCESS {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Princess();
         }
     },
     THIEF {
         @Override
-        public it.polimi.ingsw.model.cards.CharacterCard instantiate() {
+        public CharacterCard instantiate() {
             return new Thief();
         }
     };
 
-    public abstract it.polimi.ingsw.model.cards.CharacterCard instantiate();
+    public abstract CharacterCard instantiate();
 }
